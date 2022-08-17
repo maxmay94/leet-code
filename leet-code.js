65a7f3310c26e2dc69b24d8cb0091986b74a3a4a
maxmay94 <maxmay94@gmail.com>
2022-08-17 18:06:00 -0400

    solve 387. first unique character in a string

--- leet-code.js
+++ leet-code.js
@@ -35,11 +35,38 @@ var uniqueMorseRepresentations = function(words) {
   })
   
   return ret.size
-};
+}
 
 /*
   Runtime: 65 ms, faster than 96.35% of JavaScript online submissions for Unique Morse Code Words.
   Memory Usage: 43.1 MB, less than 68.25% of JavaScript online submissions for Unique Morse Code Words.
+*/
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+/* 
+  387. First Unique Character in a String
+  Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+*/
+
+var firstUniqChar = function(s) {
+  s = s.split('')
+  const charMap = new Map()
+  
+  s.forEach(char => {
+      if(!charMap.has(char)) charMap.set(char, 1)
+      else charMap.set(char, charMap.get(char) + 1)
+  })
+  
+  for(let i in s) {
+      if(charMap.get(s[i]) === 1) return i
+  }
+  return -1
+}
+
+/*
+  Runtime: 216 ms, faster than 14.67% of JavaScript online submissions for First Unique Character in a String.
+  Memory Usage: 57.4 MB, less than 5.18% of JavaScript online submissions for First Unique Character in a String.
 */
 
 // ------------------------------------------------------------------------------------------------------------------------------------------------------------------------
