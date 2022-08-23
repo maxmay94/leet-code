8e8b93cdc1be181d6ca274d3313a78e5482bb78a
maxmay94 <maxmay94@gmail.com>
2022-08-23 13:49:29 -0400

    solve Convert number to reversed array of digits

--- code-wars.py
+++ code-wars.py
@@ -7,3 +7,13 @@ def dna_to_rna(dna):
     return dna.replace('T', 'U')
 
 # ------------------------------------------------------------------------------------------------------------
+
+"""
+  Convert number to reversed array of digits
+  Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+"""
+
+def digitize(n):
+    return [int(s) for s in str(n)][::-1]
+
+# ------------------------------------------------------------------------------------------------------------
