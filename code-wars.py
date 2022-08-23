96a74ea25b14ea20fa7ed03f1d9c0d153d016fb0
maxmay94 <maxmay94@gmail.com>
2022-08-23 13:37:57 -0400

    solve DNA to RNA Conversion

---
+++ code-wars.py
@@ -0,0 +1,9 @@
+"""
+  DNA to RNA Conversion
+  Convert any 'T' in a string to a 'U'
+  "GCAT"  =>  "GCAU"
+"""
+def dna_to_rna(dna):
+    return dna.replace('T', 'U')
+
+# ------------------------------------------------------------------------------------------------------------
