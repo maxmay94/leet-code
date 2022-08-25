7663a05f23265085c4e76b426a29b5c1d671c722
maxmay94 <maxmay94@gmail.com>
2022-08-25 12:32:37 -0400

    solve List Filtering

--- code-wars.py
+++ code-wars.py
@@ -219,6 +219,24 @@ Even or Odd
 """
 
 def even_or_odd(number):
-    return 'Even' if number % 2 == 0 else 'Odd'
+    return 'Odd' if number % 2 else 'Even'
+
+#Interesting solution
+def even_or_odd_2(number):
+  return ["Even", "Odd"][number % 2]
+
+# ------------------------------------------------------------------------------------------------------------
+
+"""
+List Filtering
+  In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+    Example
+    filter_list([1,2,'a','b']) == [1,2]
+    filter_list([1,'a','b',0,15]) == [1,0,15]
+    filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+"""
+
+def filter_list(l):
+  return [i for i in l if not isinstance(i, str)]
 
 # ------------------------------------------------------------------------------------------------------------
