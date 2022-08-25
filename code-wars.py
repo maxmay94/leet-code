4146cbe56611fa26c4e76f4f61aab1ded2820a0e
maxmay94 <maxmay94@gmail.com>
2022-08-25 12:51:10 -0400

    solve Binary Addition

--- code-wars.py
+++ code-wars.py
@@ -252,3 +252,17 @@ def smash(words):
     return ' '.join(words)
 
 # ------------------------------------------------------------------------------------------------------------
+
+"""
+Binary Addition
+  Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+  The binary number returned should be a string.
+    Examples:(Input1, Input2 --> Output (explanation)))
+    1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
+    5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
+"""
+
+def add_binary(a,b):
+    return bin(a + b)[2:]
+
+# ------------------------------------------------------------------------------------------------------------
