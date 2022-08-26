475462f03775b26cacaaa2374f00f9dafac79220
maxmay94 <maxmay94@gmail.com>
2022-08-26 11:17:24 -0400

    solve Remove First and Last Character

--- code-wars.py
+++ code-wars.py
@@ -300,3 +300,13 @@ def double_char(s):
     return ''.join(char + char for char in s)
 
 # ------------------------------------------------------------------------------------------------------------
+
+"""
+Remove First and Last Character
+  It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+"""
+
+def remove_char(s):
+    return s[1:-1]
+
+# ------------------------------------------------------------------------------------------------------------
