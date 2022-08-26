0cbe3c8279ad06331a963098a9f44985d5d63022
maxmay94 <maxmay94@gmail.com>
2022-08-26 11:25:38 -0400

    solve Area or Perimeter

--- code-wars.py
+++ code-wars.py
@@ -331,3 +331,17 @@ def find_average(numbers):
     return sum(numbers) / len(numbers)
 
 # ------------------------------------------------------------------------------------------------------------
+
+"""
+Area or Perimeter
+  You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+  If it is a square, return its area. If it is a rectangle, return its perimeter.
+    Example(Input1, Input2 --> Output):
+    6, 10 --> 32
+    3, 3 --> 9
+"""
+
+def area_or_perimeter(l , w):
+    return l * w if l == w else (l * 2) + (w * 2)
+
+# ------------------------------------------------------------------------------------------------------------
