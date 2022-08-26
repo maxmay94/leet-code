f26117f9b9bca712a51e4b9337afc5954d724eaa
maxmay94 <maxmay94@gmail.com>
2022-08-26 11:22:07 -0400

    solve Calculate average

--- code-wars.py
+++ code-wars.py
@@ -320,3 +320,14 @@ def make_upper_case(s):
     return s.upper()
 
 # ------------------------------------------------------------------------------------------------------------
+
+"""
+Calculate average
+  Write a function which calculates the average of the numbers in a given list.
+    Note: Empty arrays should return 0.
+"""
+
+def find_average(numbers):
+    return sum(numbers) / len(numbers)
+
+# ------------------------------------------------------------------------------------------------------------
