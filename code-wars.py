1870c40bf4bfcb444d7e6ab708125c88b4242a16
maxmay94 <maxmay94@gmail.com>
2022-08-26 11:47:37 -0400

    solve Count by X

--- code-wars.py
+++ code-wars.py
@@ -370,3 +370,18 @@ def isValidWalk_2(walk):
     return len(walk) == 10 and walk.count('n') == walk.count('s') and walk.count('e') == walk.count('w')
 
 # ------------------------------------------------------------------------------------------------------------
+
+"""
+Count by X
+  Create a function with two arguments that will return an array of the first (n) multiples of (x).
+  Assume both the given number and the number of times to count will be positive numbers greater than 0.
+  Return the results as an array (or list in Python, Haskell or Elixir).
+    Examples:
+      count_by(1,10) #should return [1,2,3,4,5,6,7,8,9,10]
+      count_by(2,5) #should return [2,4,6,8,10]
+"""
+
+def count_by(x, n):
+    return [x * (i + 1) for i in range(0, n)]
+
+# ------------------------------------------------------------------------------------------------------------
